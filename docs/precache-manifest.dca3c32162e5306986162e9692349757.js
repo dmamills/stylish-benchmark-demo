0e11e4e992ef743d94ce5bb761a681c6e2ad174a
daniel mills <get@yomills.com>
2020-03-08 11:01:59 -0400

    add built version

---
+++ docs/precache-manifest.dca3c32162e5306986162e9692349757.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "2bf830d49a8849a14bae",
+    "url": "/static/js/main.2bf830d4.chunk.js"
+  },
+  {
+    "revision": "3ac22ad3141dd22a241b",
+    "url": "/static/js/1.3ac22ad3.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f2055d3b7aac30c9097a8bd574355463",
+    "url": "/index.html"
+  }
+];
