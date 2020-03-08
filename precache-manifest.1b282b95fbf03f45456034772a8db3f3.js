113d61a5c3eb639820fe4bd23a5b2482e1d03835
daniel mills <get@yomills.com>
2020-03-08 12:46:00 -0400

    Updates

---
+++ precache-manifest.1b282b95fbf03f45456034772a8db3f3.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "2bf830d49a8849a14bae",
+    "url": "/sdemo/static/js/main.2bf830d4.chunk.js"
+  },
+  {
+    "revision": "3ac22ad3141dd22a241b",
+    "url": "/sdemo/static/js/1.3ac22ad3.chunk.js"
+  },
+  {
+    "revision": "85794310000b93eb2cff",
+    "url": "/sdemo/static/js/runtime~main.85794310.js"
+  },
+  {
+    "revision": "dbe9c110b6d337a9a4f31b908271c2ac",
+    "url": "/sdemo/index.html"
+  }
+];
